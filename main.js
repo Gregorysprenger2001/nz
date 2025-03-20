193c4f0e4ad9a1bd30621b8b9b721a1e0e65ac7a
Gregory Sprenger <gregory.sprenger@student.uibk.ac.at>
2025-03-20 18:05:59 +0100

    script

--- main.js
+++ main.js
@@ -1,1 +1,7 @@
-console.log("Hallo!");
+var map = L.map('map').setView([-37.882778, 175.636667], 11);
+        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+            maxZoom: 19,
+            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        }).addTo(map);
+        var marker = L.marker([-37.882778, 175.636667]).addTo(map);
+        marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
