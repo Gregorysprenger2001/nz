8ec778786211bfe8510b83024c2d9304ef908c1a
Gregory Sprenger <gregory.sprenger@student.uibk.ac.at>
2025-03-27 15:36:50 +0100

    kommentare ergänzt

--- main.js
+++ main.js
@@ -1,7 +1,23 @@
+/*
+Skript für Neuseelandreie
+
+*/
+
+//Karte initialisieren
+
+
 var map = L.map('map').setView([-37.882778, 175.636667], 11);
+
+//Hintergrundkarte definieren
+
         L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
             maxZoom: 19,
             attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
         }).addTo(map);
+
+        //Marker zeichnen
+        
         var marker = L.marker([-37.882778, 175.636667]).addTo(map);
+        //Popup definieren und öffnen
+        
         marker.bindPopup("<b>Hello world!</b><br>I am a popup.").openPopup();
