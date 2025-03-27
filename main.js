4380e68c045a1324c5dbd0f28e03ded0fd1d4945
Gregory Sprenger <gregory.sprenger@student.uibk.ac.at>
2025-03-27 17:46:25 +0100

    Marker and popup für alle Etappen zeichnen

--- main.js
+++ main.js
@@ -6,10 +6,10 @@ Skript für Neuseelandreie
 //Karte initialisieren
 
 
-let stop= {  
+let stop = {
     nr: 4,
     title: "Cormandel Peninsula",
-    usr:"Gregorysprenger2001",
+    usr: "Gregorysprenger2001",
     lat: -37.882778,
     lng: 175.636667,
     zoom: 11,
@@ -27,9 +27,9 @@ const STOPS = [
         nr: 2,
         title: "Bay of Islands",
         user: "Ellinnaa",
-        lat:-35.183333,
-        lng:174.166667,
-        zoom:11,
+        lat: -35.183333,
+        lng: 174.166667,
+        zoom: 11,
     },
     {
         nr: 3,
@@ -39,10 +39,10 @@ const STOPS = [
         lng: 174.8,
         zoom: 13,
     },
-     {  
+    {
         nr: 4,
         title: "Cormandel Peninsula",
-        usr:"Gregorysprenger2001",
+        usr: "Gregorysprenger2001",
         lat: -37.882778,
         lng: 175.636667,
         zoom: 11,
@@ -83,19 +83,19 @@ const STOPS = [
         nr: 9,
         title: "Wellington",
         user: "cs4151",
-        lat: "-41.2875",
+        lat: -41.2875,
         lng: 174.776111,
         zoom: 12,
     },
     {
-      title: "Picton",
-      user: "pauly0602",
-      nr: 10,
-      lat: -41.29,
-      lng: 174.0,
-      zoom: 11,
-      },
-    {  
+        title: "Picton",
+        user: "pauly0602",
+        nr: 10,
+        lat: -41.29,
+        lng: 174.0,
+        zoom: 11,
+    },
+    {
         nr: 11,
         title: "Nelson",
         user: "lujehle",
@@ -124,16 +124,16 @@ const STOPS = [
         title: "Wanaka",
         user: "lizzie2911",
         lat: -44.7,
-        lng:169.15,
+        lng: 169.15,
         zoom: 13,
     },
     {
-      nr: 17,
-      title: "Queenstown",
-      user: "Kathleenuniibk",
-      lat: -45.031389,
-      lng: 168.660833,
-      zoom:14,
+        nr: 17,
+        title: "Queenstown",
+        user: "Kathleenuniibk",
+        lat: -45.031389,
+        lng: 168.660833,
+        zoom: 14,
     },
     {
         nr: 18,
@@ -169,7 +169,7 @@ const STOPS = [
     },
     {
         nr: 24,
-        titel: "Moeraki Boulders",
+        title: "Moeraki Boulders",
         user: "StephanPumpernik",
         lat: -45.345275,
         lng: 170.826061,
@@ -184,7 +184,7 @@ const STOPS = [
         user: "PriPh625",
     },
     {
-    title: "Mount Cook",
+        title: "Mount Cook",
         nr: 26,
         user: "Basti-10",
         zoom: 13,
@@ -199,27 +199,38 @@ const STOPS = [
         lng: 173.682222,
         zoom: 11
     },
-    ];
- 
+];
+console.log(STOPS[0])
+console.log(STOPS[0].title)
+
+
+//Karte initialisieren
+
 let map = L.map('map').setView([stop.lat, stop.lng], stop.zoom);
 
 //Hintergrundkarte definieren
 
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
-        }).addTo(map);
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
 
-        //Marker zeichnen
+//loop über Etappen
+for (let i = 0; i < STOPS.length; i++) {
+    console.log(STOPS[i])
 
-        let marker = L.marker([stop.lat, stop.lng]).addTo(map);
-        //Popup definieren und öffnen
-        
-            marker.bindPopup(`
+    //Marker zeichnen
+    let marker = L.marker([STOPS[i].lat, STOPS[i].lng]).addTo(map);
+    //Popup definieren und öffnen
+
+marker.bindPopup(`
     
-                <h2>${stop.title}</h2>
-                <ul>
-                <li> Geogr. Breite: ${stop.lat.toFixed(5)}°</li>
-                <li> Geogr. Länge: ${stop.lng.toFixed(5)}°</li>
-                </ul>
-                `).openPopup();
+    <h2>${STOPS[i].title}</h2>
+    <ul>
+    <li> Geogr. Breite: ${STOPS[i].lat.toFixed(5)}°</li>
+    <li> Geogr. Länge: ${STOPS[i].lng.toFixed(5)}°</li>
+    </ul>
+    `).openPopup();
+
+};
+
